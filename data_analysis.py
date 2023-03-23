f02ca40885faff968f42f203f0fffec3bf2c1447
Khaled Ameen <Khaled.ameen11@gmail.com>
2023-03-23 21:51:41 +0100

    Using sqlalchemy instead of mysql.connector as connector to MySQL database

--- data_analysis.py
+++ data_analysis.py
@@ -1,21 +1,24 @@
-import mysql.connector
+import sqlalchemy as db
 import pandas as pd
 import numpy as np
 
 # Connect to MySQL database
-cnx = mysql.connector.connect(user='root', password='2020$2020$ABC',
-                              host='127.0.0.1', database='clash_royale_database')
-
+engine = db.create_engine('mysql+mysqlconnector://root:2020$2020$ABC@127.0.0.1/clash_royale_database')
+connection = engine.connect()
 
 # Execute SQL query and fetch data into a DataFrame
 query = "SELECT * FROM player_stats"
-df = pd.read_sql_query(query, cnx)
+df = pd.read_sql_query(query, connection)
 
 # Perform data analysis operations on the DataFrame using Pandas and NumPy functions
-mean = np.mean(df['column_name'])
-std_dev = np.std(df['column_name'])
+mean = np.mean(df['net_win_rate'])
+std_dev = np.std(df['net_win_rate'])
+
+print(mean)
+print(std_dev)
 
 # Close MySQL connection
-cnx.close()
+connection.close()
+engine.dispose()
 
 exit()
