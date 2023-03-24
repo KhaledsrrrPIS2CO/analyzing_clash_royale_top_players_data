b3350d96cf3e74dc7390ca01b66dbe110c0b55bb
Khaled Ameen <Khaled.ameen11@gmail.com>
2023-03-24 14:45:02 +0100

    Calculate the 90th percentile of net_win_rate using pandas then get the top 10% of the net_win_rate from MySQL

--- data_analysis.py
+++ data_analysis.py
@@ -1,24 +1,33 @@
-import sqlalchemy as db
+import mysql.connector
 import pandas as pd
 import numpy as np
 
 # Connect to MySQL database
-engine = db.create_engine('mysql+mysqlconnector://root:2020$2020$ABC@127.0.0.1/clash_royale_database')
-connection = engine.connect()
+cnx = mysql.connector.connect(user='root', password='2020$2020$ABC',
+                              host='127.0.0.1', database='clash_royale_database')
+
 
 # Execute SQL query and fetch data into a DataFrame
 query = "SELECT * FROM player_stats"
-df = pd.read_sql_query(query, connection)
+df = pd.read_sql_query(query, cnx)
 
 # Perform data analysis operations on the DataFrame using Pandas and NumPy functions
 mean = np.mean(df['net_win_rate'])
 std_dev = np.std(df['net_win_rate'])
+# Calculate the 90th percentile of net_win_rate
+top_10_percentile = df['net_win_rate'].quantile(0.9)
+# Get the top 10% of players based on net_win_rate
+top_players = df[df['net_win_rate'] >= top_10_percentile]
+
 
 print(mean)
 print(std_dev)
+print(top_players)
 
 # Close MySQL connection
-connection.close()
-engine.dispose()
+cnx.close()
+
 
 exit()
+
+
