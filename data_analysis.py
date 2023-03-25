7bc3da5b18dac2b89aa7f1468b7720ca18e2291c
Khaled Ameen <Khaled.ameen11@gmail.com>
2023-03-25 21:37:43 +0100

    descriptive statistics: get_measures_variability

--- data_analysis.py
+++ data_analysis.py
@@ -14,7 +14,7 @@ def get_measures_central_tendency():
     df = pd.read_sql_query(query, cnx)
 
     # Perform data analysis operations on the DataFrame using Pandas and NumPy functions
-    mean = np.mean(df['net_win_rate'])
+    mean = round(np.mean(df['net_win_rate']),2)
     median = np.median(df['net_win_rate'])
     mode = stats.mode(df['net_win_rate'])[0][0]
 
@@ -24,5 +24,33 @@ def get_measures_central_tendency():
     return mean, median, mode
 
 
-measures_central_tendency = get_measures_central_tendency()
-print(measures_central_tendency)
+mean, median, mode = get_measures_central_tendency()
+print(f"\nMean: {mean} \nMedian: {median} \nMode: {mode}")
+
+
+def get_measures_variability():
+    # Connect to MySQL database
+    cnx = mysql.connector.connect(user='root', password='2020$2020$ABC',
+                                  host='127.0.0.1', database='clash_royale_database')
+
+    # Execute SQL query and fetch data into a DataFrame
+    query = "SELECT net_win_rate FROM player_stats"
+    df = pd.read_sql_query(query, cnx)
+
+    # Calculate the range, variance, and standard deviation of net_win_rate
+    range_val = np.max(df['net_win_rate']) - np.min(df['net_win_rate'])
+    variance_val = round(np.var(df['net_win_rate']), 2)
+    std_dev = round(np.std(df['net_win_rate']), 2)
+
+    # Close MySQL connection
+    cnx.close()
+
+    # Return the results as a dictionary
+    return {range_val, variance_val, std_dev}
+
+
+range_val, variance_val, std_dev = get_measures_variability()
+print(f"Range: {range_val} \nVariance: {variance_val} \nStd: {std_dev}")
+
+
+
